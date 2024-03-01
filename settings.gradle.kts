c5914a5bc114731c320e2c1d49c5d21dcaa3d368
Christian Kohlstedde <christian@kohlsted.de>
2024-03-01 10:19:57 +0100

    Disable all plugins except one to improve build performance

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,17 +11,4 @@ includeBuild("BlueMapCommon")
 // implementations
 includeBuild("implementations/cli")
 
-includeBuild("implementations/fabric-1.18")
-includeBuild("implementations/fabric-1.19.4")
-includeBuild("implementations/fabric-1.20")
-
-includeBuild("implementations/forge-1.18.1")
-includeBuild("implementations/forge-1.19.4")
-includeBuild("implementations/forge-1.20")
-
-includeBuild("implementations/neoforge-1.20.2")
-
-includeBuild("implementations/spigot")
 includeBuild("implementations/paper")
-
-includeBuild("implementations/sponge")
